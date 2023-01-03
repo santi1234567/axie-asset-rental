fdee4d777109eb659a958f2725b46b50b5d4d418
santi1234567 <45318759+santi1234567@users.noreply.github.com>
2023-01-03 18:13:52 -0300

    Added basic functions

--- contract/contracts/Rental.sol
+++ contract/contracts/Rental.sol
@@ -10,10 +10,15 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
+import "./IRERC721.sol";
+
 contract Rental is Ownable, IERC721Receiver {
     using Counters for Counters.Counter;
+    string defaultTokenURI = "https://ronin.rest/ronin/axie/9953568";
 
-    mapping(address => mapping(uint256 => bool)) assetDeposited; // Lists deposited assets by address
+    address RENTED_ERC721_CONTRACT = address(0);
+
+    mapping(address => mapping(uint256 => bool)) assetDeposited; // Indicates if asset is deposited
 
     struct rent {
         address assetContract;
@@ -23,11 +28,18 @@ contract Rental is Ownable, IERC721Receiver {
         uint256 pricePerDay;
         address assetOwner;
         address rentedBy;
-        uint256 rentExpireBlock;
+        uint256 rentExpireBlockTimeStamp;
+        bool active;
     }
 
     Counters.Counter rentIdCounter;
     mapping(uint256 => rent) rents; // List of rents by rentId
+
+    mapping(address => mapping(uint256 => bool)) assetOnActiveRent; // Indicates if asset has an active rent
+
+    constructor(address _rentedERC721Contract) {
+        RENTED_ERC721_CONTRACT = _rentedERC721Contract;
+    }
 
     // Public functions
 
@@ -43,13 +55,27 @@ contract Rental is Ownable, IERC721Receiver {
 
     /**
      * @dev Deposits an asset in the contract for lending. The rental contract requires approval from the asset contract.
-     * @param _assetContract The contract address of the asset to be rented.
-     * @param _assetId The Id of the asset to be rented.
+     * @param _assetContract The contract address of the asset to be deposited.
+     * @param _assetId The Id of the asset to be deposited.
      **/
     function depositAsset(address _assetContract, uint256 _assetId) public {
         IERC721 assetContract = IERC721(_assetContract);
         assetContract.safeTransferFrom(msg.sender, address(this), _assetId);
         assetDeposited[_assetContract][_assetId] = true;
+    }
+
+    /**
+     * @dev Retrieves an asset from the contract for lending. The rental contract requires approval from the asset contract.
+     * @param _assetContract The contract address of the asset to be retrieved.
+     * @param _assetId The Id of the asset to be retrieved.
+     **/
+    function retrieveAsset(address _assetContract, uint256 _assetId) public {
+        require(assetDeposited[_assetContract][_assetId]);
+        require(!assetOnActiveRent[_assetContract][_assetId]);
+
+        IERC721 assetContract = IERC721(_assetContract);
+        assetContract.safeTransferFrom(address(this), msg.sender, _assetId);
+        assetDeposited[_assetContract][_assetId] = false;
     }
 
     /**
@@ -64,9 +90,73 @@ contract Rental is Ownable, IERC721Receiver {
         uint256 _maxDays,
         uint256 _pricePerDay
     ) public {
+        require(assetDeposited[_assetContract][_assetId]);
+        require(!assetOnActiveRent[_assetContract][_assetId]);
+        //IERC721 assetContract = IERC721(_assetContract);
         rent storage newRent = rents[rentIdCounter.current()];
-        rentIdCounter.increment();
+        newRent.assetContract = _assetContract;
+        newRent.assetId = _assetId;
         newRent.assetOwner = msg.sender;
-        newRent.depositedAsset = depositedAsset(_assetContract, _assetId);
+        newRent.minDays = _minDays;
+        newRent.maxDays = _maxDays;
+        newRent.pricePerDay = _pricePerDay;
+        newRent.active = true;
+        rentIdCounter.increment();
+        assetOnActiveRent[_assetContract][_assetId] = true;
+    }
+
+    /**
+     * @dev Ends a rental. Requires the asset to be deposited in the contract.
+     * @param rentId The Id of the rent to be ended.
+     **/
+    function endRent(uint256 rentId) public {
+        rent storage activeRent = rents[rentId];
+        require(activeRent.assetOwner == msg.sender);
+        require(assetDeposited[activeRent.assetContract][activeRent.assetId]);
+        require(
+            assetOnActiveRent[activeRent.assetContract][activeRent.assetId]
+        );
+        assetOnActiveRent[activeRent.assetContract][activeRent.assetId] = true;
+        activeRent.active = false;
+    }
+
+    /**
+     * @dev Rents an asset. Requires the asset to be deposited in the contract.
+     * @param rentId The Id of the rent to be ended.
+     **/
+    function rentAsset(uint256 rentId, uint256 rentDays) public {
+        rent storage activeRent = rents[rentId];
+        require(activeRent.active);
+        require(
+            rentDays >= activeRent.minDays && rentDays <= activeRent.maxDays
+        );
+        activeRent.rentedBy = msg.sender;
+        activeRent.rentExpireBlockTimeStamp = block.timestamp + rentDays;
+
+        IRERC721 rentedERC721Contract = IRERC721(RENTED_ERC721_CONTRACT);
+        try rentedERC721Contract.ownerOf(activeRent.assetId) returns (
+            address assetOwner
+        ) {
+            rentedERC721Contract.safeTransferFrom(
+                assetOwner,
+                msg.sender,
+                activeRent.assetId
+            );
+        } catch Error(string memory reason) {
+            require(
+                keccak256(abi.encodePacked(reason)) ==
+                    keccak256(
+                        abi.encodePacked(
+                            "Returned error: execution reverted: ERC721: invalid token ID"
+                        )
+                    ),
+                reason
+            );
+            rentedERC721Contract.safeMint(
+                msg.sender,
+                defaultTokenURI,
+                activeRent.assetId
+            );
+        }
     }
 }
