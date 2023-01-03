9db52bc48c1468bf949582b0dc390640754edc93
santi1234567 <45318759+santi1234567@users.noreply.github.com>
2023-01-03 18:14:07 -0300

    Created contract for rented erc721

---
+++ contract/contracts/RentedERC721.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract MyToken is ERC721, ERC721Enumerable, ERC721URIStorage, AccessControl {
+    bytes32 public constant RENTAL_CONTRACT = keccak256("RENTAL_CONTRACT");
+
+    constructor(address rentalContract) ERC721("RentedERC721", "RERC721") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(RENTAL_CONTRACT, rentalContract);
+    }
+
+    function safeMint(
+        address to,
+        string memory uri,
+        uint256 tokenId
+    ) public onlyRole(RENTAL_CONTRACT) {
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId,
+        uint256 batchSize
+    ) internal override(ERC721, ERC721Enumerable) onlyRole(RENTAL_CONTRACT) {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
+    {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
