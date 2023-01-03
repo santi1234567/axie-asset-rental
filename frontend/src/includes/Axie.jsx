5d7eecb96627ddf2b9890d25790068ae6acc60f3
Esteban-Somoza <estfersomoza@gmail.com>
2023-01-03 16:04:09 -0300

    css on marketplace page

--- frontend/src/includes/Axie.jsx
+++ frontend/src/includes/Axie.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import React, {useContext} from 'react'
+import { ethPrice } from '../context/ethPriceContext'
 
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import './axie.css'
 
 export default function Axie({ data }) {
+    const { eth, setEth } = useContext(ethPrice)
+
     return (
         <Link className='axie' to={`/axie/${data.id}`}>
             <div >
                 <img src={`images/${data.image}.png`} alt="" />
-                <h3>
-                    Id: {data.id}
-                </h3>
-                <h3>
-                    price: {data.id}
-                </h3>
+                <div className='axieInfo'>
+                    <h4>Axie ID: #{data.id}</h4>
+                    <span>
+                        <h3 className='eth'>
+                            price: E{data.price}
+                        </h3>
+                    </span>
+                    <span>
+                        <h3 className='usd'>
+                            price: ${data.price*eth}
+                        </h3>
+                    </span>
+                </div>
             </div>
         </Link>
     )
